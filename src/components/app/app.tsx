2c94d61819cdfc520c0bd161072f0add13868764
Sean Baines <mrseanbaines@gmail.com>
2020-02-12 23:08:25 +0000

    Refactor auth flow

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,36 +1,9 @@
 import React from 'react'
-import uuidv4 from 'uuid/v4'
 
 const App: React.FC = () => {
-  const {
-    REACT_APP_MONZO_AUTH_URL = '',
-    REACT_APP_CLIENT_ID = '',
-    REACT_APP_REDIRECT_PATH = '',
-    REACT_APP_SITE_URL = '',
-  } = process.env
+  const { REACT_APP_API_URL = '' } = process.env
 
-  const handleLoginClick = (e: React.MouseEvent) => {
-    e.preventDefault()
-
-    const uuid = uuidv4()
-
-    const query = new URLSearchParams({
-      client_id: REACT_APP_CLIENT_ID,
-      redirect_uri: REACT_APP_SITE_URL + REACT_APP_REDIRECT_PATH,
-      response_type: 'code',
-      state: uuid,
-    })
-
-    window.location.href = `${REACT_APP_MONZO_AUTH_URL}/?${query}`
-
-    localStorage.setItem('state', uuid)
-  }
-
-  return (
-    <button type='button' onClick={handleLoginClick}>
-      Login
-    </button>
-  )
+  return <a href={`${REACT_APP_API_URL}/auth/login`}>Login</a>
 }
 
 export default App
