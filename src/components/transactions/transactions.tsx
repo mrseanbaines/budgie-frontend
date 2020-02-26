4bca3ae7c87ca6cad49315cf2a3e31b5f4e9fc4c
Sean Baines <mrseanbaines@gmail.com>
2020-02-26 20:45:01 +0000

    Add filter sidebar with search filter

--- src/components/transactions/transactions.tsx
+++ src/components/transactions/transactions.tsx
@@ -1,13 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { RouteComponentProps } from 'react-router-dom'
-import { List, Row, Col } from 'antd'
+import { Layout as AntLayout, List, Row, Col, Input } from 'antd'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { fetchTransactions } from 'store/transactions/actions'
 import { fetchCategories } from 'store/categories/actions'
 import { getTransactionItems } from 'store/transactions/selectors'
+import { Transaction as TransactionType } from 'store/transactions/types'
 import Transaction from 'components/transaction'
 import Layout from 'components/layout'
+
+const { Sider, Content } = AntLayout
+const { Search } = Input
 
 interface Params {
   date: string
@@ -19,23 +23,45 @@ const Transactions: React.FC<Props> = ({ match }) => {
   const { date } = match.params
   const transactions = useSelector(getTransactionItems)
   const dispatch = useDispatch()
+  const [searchQuery, setSearchQuery] = useState('')
 
   useEffect(() => {
     dispatch(fetchTransactions(date))
     dispatch(fetchCategories())
   }, [dispatch, date])
 
+  const searchFilter = (transaction: TransactionType) => {
+    return (
+      (typeof transaction.merchant === 'object' &&
+        transaction.merchant.name.toLowerCase().includes(searchQuery.toLowerCase())) ||
+      (typeof transaction.merchant === 'string' &&
+        transaction.merchant.toLowerCase().includes(searchQuery.toLowerCase()))
+    )
+  }
+
   return (
     <Layout date={date} backTo={`/${date}/overview`} currentPage='transactions'>
-      <Row type='flex' justify='center'>
-        <Col span={10}>
-          <List
-            size='small'
-            dataSource={transactions}
-            renderItem={transaction => <Transaction transaction={transaction} />}
+      <AntLayout>
+        <Sider width={300} theme='light' style={{ padding: '1rem' }}>
+          <Search
+            allowClear
+            placeholder='Search for a merchant'
+            onChange={({ target: { value } }) => setSearchQuery(value)}
           />
-        </Col>
-      </Row>
+        </Sider>
+
+        <Content style={{ minHeight: '100%' }}>
+          <Row type='flex' justify='center'>
+            <Col span={10}>
+              <List
+                size='small'
+                dataSource={transactions.filter(searchFilter)}
+                renderItem={transaction => <Transaction transaction={transaction} />}
+              />
+            </Col>
+          </Row>
+        </Content>
+      </AntLayout>
     </Layout>
   )
 }
