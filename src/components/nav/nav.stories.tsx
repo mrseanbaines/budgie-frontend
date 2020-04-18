5d48e48b8b0c4c7bcb8af4b94f8d64ed0849658e
Sean Baines <mrseanbaines@gmail.com>
2020-04-18 08:14:07 +0100

    Fix React Router Storybook error

--- src/components/nav/nav.stories.tsx
+++ src/components/nav/nav.stories.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
 
 import Nav from './nav'
 
 export default {
   title: 'Nav',
+  decorators: [(getStory: Function) => <MemoryRouter>{getStory()}</MemoryRouter>],
 }
 
 export const Default = () => <Nav />
