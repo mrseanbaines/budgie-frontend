5d050ab51e184ff7a9db1dd95b106996b7205403
Sean Baines <mrseanbaines@gmail.com>
2020-05-19 20:04:28 +0100

    Add layout to profile page

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 
+import Layout from 'components/layout'
+import Header from 'components/header'
 import { logout } from 'store/auth/actions'
+import * as s from 'styles/common'
 
 const Profile: React.FC = () => {
   const dispatch = useDispatch()
@@ -10,7 +13,17 @@ const Profile: React.FC = () => {
     dispatch(logout())
   }
 
-  return <button onClick={handleLogout}>Log out</button>
+  return (
+    <s.Wrapper>
+      <s.HeaderWrapper>
+        <Header title='Profile' />
+      </s.HeaderWrapper>
+
+      <Layout>
+        <button onClick={handleLogout}>Log out</button>
+      </Layout>
+    </s.Wrapper>
+  )
 }
 
 export default Profile
